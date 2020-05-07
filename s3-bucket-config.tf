f8ec54f33664ecfc4facceb26f356683b2ea71ad
Helder Klemp <helderklemp@gmail.com>
2020-05-07 15:26:33 +1000

    add s3 config logs bucket

---
+++ s3-bucket-config.tf
@@ -0,0 +1,94 @@
+data "aws_iam_policy_document" "s3_policy_config" {
+  statement {
+    sid    = "ConfigLogs"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.org_name}-audit-config-${data.aws_region.current.name}/*"
+    ]
+  }
+  statement {
+    sid    = "OrgAccounts"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = formatlist("arn:aws:iam::%s:root", var.account_ids)
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.org_name}-audit-config-${data.aws_region.current.name}/*"
+    ]
+  }
+  statement {
+    sid    = "OrgAccountsAcl"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = formatlist("arn:aws:iam::%s:root", var.account_ids)
+    }
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:PutBucketAcl"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.org_name}-audit-config-${data.aws_region.current.name}"
+    ]
+  }
+  statement {
+    sid    = "ConfigLogsAcl"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.org_name}-audit-config-${data.aws_region.current.name}"
+    ]
+  }
+}
+
+resource "aws_s3_bucket" "config" {
+  bucket = "${var.org_name}-audit-config-${data.aws_region.current.name}"
+  acl    = "private"
+  policy = data.aws_iam_policy_document.s3_policy_config.json
+
+  lifecycle {
+    ignore_changes = [
+      versioning,
+      grant
+    ]
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id      = "ARCHIVING"
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days          = var.s3_days_until_glacier
+      storage_class = "GLACIER"
+    }
+  }
+}
